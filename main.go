171c7d327f7ba128a6c34c6f46d3300b35dcae79
Grant <Grant.Lawton@gmail.com>
2025-06-15 22:11:56 +1000

    fixed linting error on main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/mantisec/terraform-provider-umbrella/internal/provider"
@@ -11,5 +12,8 @@ import (
 // Provider entry point
 // -----------------------------------------------------------------------------
 func main() {
-	providerserver.Serve(context.Background(), provider.NewProvider, providerserver.ServeOpts{})
+	err := providerserver.Serve(context.Background(), provider.NewProvider, providerserver.ServeOpts{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
